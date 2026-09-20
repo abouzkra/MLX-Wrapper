44077c2e6656d6a9707b172e00998f69a45689a9
abouzkra <adnanbouzkraoui037@gmail.com>
2026-09-20 22:50:08 +0000

    Loop Single-Frame Animations Automatically

--- mlx_wrapper/asset/animated_sprite.py
+++ mlx_wrapper/asset/animated_sprite.py
@@ -57,6 +57,9 @@ class AnimatedSprite(Asset):
         if fps <= 0:
             raise AssetError("fps must be greater than 0.")
 
+        if len(frames) == 1:
+            loop_mode = LoopMode.LOOP
+
         self.frames: list[Sprite] = frames
         self.loop_mode: LoopMode = loop_mode
 
